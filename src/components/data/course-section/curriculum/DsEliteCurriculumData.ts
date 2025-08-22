860185c0eb79bddf2cac81d570f2351ae0d5c598
gc-accounts <technology@odinschool.com>
2025-08-22 12:35:42 +0530

    data analyst curriculum data update

--- src/components/data/course-section/curriculum/DsEliteCurriculumData.ts
+++ src/components/data/course-section/curriculum/DsEliteCurriculumData.ts
@@ -1,122 +1,91 @@
 export const DsEliteCurriculumData = [
-  {
-    program_title: "Statistics & Exploratory Data Analysis",
+ {
+    program_title: "Intro to Data Science& Python Programming Basics",
     points: [
       "Foundations of Data Science",
-      "Data Wrangling and Visualization",
-      "Different types of charts & visuals",
-      "Storytelling with Data",
+"Python Basics (Data Types, Loops, Functions)",
+"Operators, Expressions",
+"Control Structures",
     ],
     logo: [
       'https://strapi.odinschool.com/uploads/Python_20_3a290a8839.webp',
-      'https://strapi.odinschool.com/uploads/Scikit_150_X60_78e109b38a.webp',
-      'https://strapi.odinschool.com/uploads/matplotlib_d282ba51f5.webp'
     ]
   },
   {
-    program_title: "Power BI",
-    points: [
-      "Data sources and connections",
-      "Data transformation and cleansing",
-      "Interactive reports and dashboards using Plotly",
-      "Visualizations, Measures and Calculated Columns, Filters, Slicers",
-    ],
+    program_title: "Data Exploration & Cleaning",
+    points: ["Introduction to NumPy & Pandas","Data cleaning techniques”,“Handling missing data, outliers, duplicates","Data wrangling and transformations",],
     logo: [
-      'https://strapi.odinschool.com/uploads/Power_20_Bi_023c537eb1.webp',
+      'https://strapi.odinschool.com/uploads/numpy_1_0e46cdf12f.webp',
+      'https://strapi.odinschool.com/uploads/Pandas_150_X60_2211de70b9.webp',
     ]
   },
   {
-    program_title: "SQL",
-    points: [
-      "Intro to databases, Basic queries, Joins and functions",
-      "Group By, Sort, Filter",
-      "Primary key, Foreign key",
-      "Working with real data",
-      "Window Functions, Common Expressions (CTEs)"
-    ],
-    logo: [
-      'https://strapi.odinschool.com/uploads/My_SQL_100_X40_9476ebdaf1.webp'
+    program_title: "Data Visualization",
+    points: ["Matplotlib for visualizing data”,“Seaborn for visualizing data” “Histograms, scatter plots, box plots", "Introduction to interactive dashboards using Plotly"],
+    logo: ['https://strapi.odinschool.com/uploads/matplotlib_1_fd4c83f90b.webp','https://strapi.odinschool.com/uploads/seaborn_webp_20_1_4dfbe48a40.webp','https://strapi.odinschool.com/uploads/Plotly_41f37ecead.webp',
     ]
   },
   {
-    program_title: "Python",
+    program_title: "SQL Basics",
     points: [
-      "Variables, Data types, Loops",
-      "Functions, Dict, Tuple, Dictionaries",
-      "Pandas and Numpy",
-      "NumPy for Numerical Computations",
-      "Exploratory Data Analysis"
+      "Introduction to databases",
+"SELECT, FROM, WHERE, Operators (including LIKE)",
+"Functions, Data Types, Primary key, foreign key",
+"Joins, Group by with Having, Aggregate functions",
     ],
     logo: [
-      "https://strapi.odinschool.com/uploads/Python_20_3a290a8839.webp",
-      "https://strapi.odinschool.com/uploads/Google_Colab_150_X60_2a6cb5dedc.webp",
-      "https://strapi.odinschool.com/uploads/Num_Py_150_X60_7660f9de30.webp",
-      "https://strapi.odinschool.com/uploads/Pandas_150_X60_2211de70b9.webp",
+      "https://strapi.odinschool.com/uploads/SQL_0b969037c0.webp",
+    ]
+  },
+  
+  {
+    program_title: "Introduction to Machine Learning",
+    points: ["Overview of ML algorithms","Classification, Regression,“Clustering, anomaly detection, and metrics.","Introduction to scikit-learn library"],
+    logo: ["https://strapi.odinschool.com/uploads/Scikit_Learn_cfe7c5c8a1.webp"]
+  },
+
+  {
+    program_title: "Supervised & Unsupervised Learning Algorithms",
+    points: ["Linear Regression and Logistic Regression","Classification Algorithms (Decision Trees, Random Forest, k-Nearest Neighbors)","Clustering","Dimensionality Reduction (PCA)"],
+    logo:['https://strapi.odinschool.com/uploads/Scikit_Learn_cfe7c5c8a1.webp', 'https://strapi.odinschool.com/uploads/Xboost_20100x40_c83e6e176d.webp', 'https://strapi.odinschool.com/uploads/Light_GBM_20100_X40_84973b6a12.webp']
+  },
+  {
+    program_title: "Deep Learning",
+    points: [
+      "Overview of Perceptron",
+"Activation Functions",
+"Mathematical Foundation",
+"CNN, RNN, LSTM",
+    ],
+    logo: [
+      "https://strapi.odinschool.com/uploads/Tensorflow_1_71401a1f5a.webp",
+      "https://strapi.odinschool.com/uploads/Py_Torch_20100_X40_e1df785b9a.webp",
+      "https://strapi.odinschool.com/uploads/Keras_139c7dd750.webp",
     ]
   },
   {
-    program_title: "Machine Learning",
+    program_title: "Natural Language Processing",
     points: [
-      "Overview: ML algorithms",
-      "Model Evaluation",
-      "Train-test split",
-      "Clustering (K-means)",
-      "Model performance metrics"
+      "NLTK,SPaCy",
+"Tokenization",
+"Word Embedding's",
+"Practical Implementation",
+
     ],
-    logo: [
-      "https://strapi.odinschool.com/uploads/Scikit_150_X60_78e109b38a.webp",
-      "https://strapi.odinschool.com/uploads/Xboost_20100x40_c83e6e176d.webp",
-      "https://strapi.odinschool.com/uploads/streamlit_20100_X40_e83b618b1c.webp"
-    ]
+
+  logo: [ 'https://strapi.odinschool.com/uploads/NLTK_150_X60_041d16cdae.webp', 'https://strapi.odinschool.com/uploads/Spacy_150_X60_e97fcaf0e8.webp']
   },
+
   {
-    program_title: "Generative AI",
+    program_title: "Data Engineering",
     points: [
-      "Prompt Engineering",
-      "Language Models",
-      "Chatbots",
-      "Ethical Considerations"
-    ],
-    logo: [
-      "https://strapi.odinschool.com/uploads/N8n_20100_X40_cd14c12c7a.webp",
-      "https://strapi.odinschool.com/uploads/Hugging_20_Face_20100_X40_20efc02ae2.svg"
-    ]
-  },
-  {
-    program_title: "Advanced Excel",
-    points: [
-      "Introduction to Excel",
-      "Data manipulation, cleaning, and formatting",
-      "Formulas and functions, Pivot tables and Charts",
-      "Data analysis using Excel"
-    ],
-    logo: [
-      "https://strapi.odinschool.com/uploads/Excel_74543af3ba.webp"
-    ]
-  },
-  {
-    program_title: "Tableau",
-    points: [
-      "Creating and Working with Different Chart Types",
-      "Data Visualizations with Filters, Hierarchies, and Drilldowns",
-      "Creating Dashboards and Stories",
-      "Advanced Functions: Parameters, Forecasting, and Relationships"
-    ],
-    logo: [
-      "https://strapi.odinschool.com/uploads/Tableau_20100_X40_dc2f10a715.webp"
-    ]
-  },
-  {
-    program_title: "Azure",
-    points: [
-      "Basics of Cloud Computing and Azure Fundamentals",
-      "Introduction to Azure SQL Database and Data Factory",
-      "Working with Azure Databricks for big data processing",
-      "Integration with Delta Lake and Azure Synapse Analytics"
-    ],
-    logo: [
-      "https://strapi.odinschool.com/uploads/Azure_bc82e74007.webp",
-      "https://strapi.odinschool.com/uploads/My_SQL_100_X40_9476ebdaf1.webp"
-    ]
+      "Extracting, transforming, and loading data",
+"Building and managing data flow systems",
+"Data Pipeline Orchestration and Automation",
+"Data Warehouses and Data Lakes",
+ ],
+
+  logo: [ 'https://strapi.odinschool.com/uploads/Apache_Spark_20100_X40_bc74935d8a.webp', 'https://strapi.odinschool.com/uploads/kafka_20100_X40_d10e5ac59f.webp']
   }
+  
 ];
